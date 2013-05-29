7ae4d77710ae81892fb7a759f18e4b6ee3738215
mil <miles.sandlar@gmail.com>
2013-05-29 18:25:54 -0400

    mapping and writing files

--- meis-wm.c
+++ meis-wm.c
@@ -5,63 +5,24 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <X11/Xlib.h>
+Display *display;
+Window root;
+int screen;
 
-void eventLoop(Display *display) {
+
+void event_loop() {
   // Variable Declarations
   XEvent event;
-  int i;
-  int fds[100];
-  int fdsLast; // Where to add the next FD in fds[]
-  int bufferSize;
-  char buffer;
-  fd_set descriptors;
-
-
-  // Setup X Fd
-  fds[0]    = ConnectionNumber(display);
-  fdsLast   = 0; 
-
-
-  for(;;) {
-    // Add file descriptors to set
-    FD_ZERO(&descriptors);
-    for (i = 0; i <= fdsLast; i++) {
-      FD_SET(fds[i], &descriptors);
-      fprintf(stderr, "Adding to fd set %d\n", i);
+  for (;;) {
+    XNextEvent(display, &event);
+    if (event.type == MapRequest) {
+      //XMapWindow( display, event.xmaprequest.window);
+      map_window(&event.xmaprequest.window);
     }
-
-    // Select from the last FD in the set +1 
-    if (select(fds[i], &descriptors, NULL, NULL, NULL)) {
-
-      /* Check xFD */
-      if (FD_ISSET(fds[0], &descriptors)) {
-        while (XPending(display) > 0) {
-          XNextEvent(display, &event);
-          if (event.type == MapRequest) {
-            mapWindowToFilesystem(event.xmaprequest.window);
-          }
-        }
-      } 
-
-      /* Handle Fifo Fds */
-      for (i = 1; i <= fdsLast; i++) {
-        if (
-            FD_ISSET(fds[i], &descriptors) && 
-            (bufferSize = read(fds[i], buffer, sizeof(buffer)) > 0)) {
-          //buffer[bufferSize] = '\0';
-        }
-      }
-    }
-
   }
-
-  // Clean up Fdsdd 
-  for (i = 0; i <= (sizeof(fds) / sizeof(fds[0])); i++)
-      close(fds[i]);
-
 }
 
-Bool createDirectory(char * path) {
+Bool create_directory(char * path) {
   struct stat buffer = {0};
   if (stat(path, &buffer) == -1) {
     mkdir(path, 0700);
@@ -70,48 +31,32 @@ Bool createDirectory(char * path) {
   return False;
 }
 
-Bool makeFifo(char * path) {
-  if (mkfifo(path, S_IRWXU) == 0) {
-    return True;
-  } else {
-    return False;
-  }
-}
 
-Bool mapWindowToFilesystem(Window * window) {
+Bool map_window(Window * window) {
   char * name;
+  fprintf(stderr, "Mapping window");
+
+  XMapWindow(display, *window);
+  XSetWindowBorderWidth(display, *window, 10);
+  XSetWindowBorder(display, *window, BlackPixel(display, screen)); 
+
 
   name = malloc(sizeof(char) * 18);
-  sprintf(name, "/tmp/meis/%d", (int) window);
+  sprintf(name, "/tmp/meis/%d", (int) *window);
 
-  if ( createDirectory(name)) {
-    free(name);
-    return True;
+  if ( create_directory(name)) {
+    free(name); return True;
   } else {
-    free(name);
-    return False;
+    free(name); return False;
   }
-}
 
-Bool mapFromFilesystem(char * path, int display) {
-
-  /*
-     XMapWindow(display, window);
-     XSetWindowBorderWidth(display, event.xmaprequest.window, 10);
-     XSetWindowBorder(display, 
-     event.xmaprequest.window, 
-     BlackPixel(display, activeScreen));
-     */
-}
+ }
 
 
 int main(int argc, char * argv[]) {
-  Display *display;
-  int screen;
-  Window root;
-
-  // Create the meis directory if doesn't already exist
-  createDirectory("/tmp/meis");
+  fprintf(stderr, "Intilaized\n");
+    // Create the meis directory if doesn't already exist
+  create_directory("/tmp/meis");
 
   // Open Display, Set Root, and Active Screen
   assert(display = XOpenDisplay(NULL));
@@ -119,13 +64,12 @@ int main(int argc, char * argv[]) {
   root   = DefaultRootWindow(display);
 
   // Select SubstructureNotify & SubstructureRedirect
-  XSelectInput(display, root, FocusChangeMask | 
-      PropertyChangeMask | SubstructureNotifyMask | 
-      SubstructureRedirectMask | KeyPressMask | ButtonPressMask);
+  XSelectInput(display, root, FocusChangeMask | PropertyChangeMask | SubstructureNotifyMask | SubstructureRedirectMask | KeyPressMask | ButtonPressMask);
   XFlush(display);
+  fprintf(stderr, "right before the event loop\n");
 
   // Call event loop to handle FIFO Fds, Inotify Events, and X Events
-  eventLoop(display);
+  event_loop();
 
   return 0;
 }
